743f3a6277b6f070e8a8634e94c831a01d460896
Tina Ulbrich <tina.ulbrich@web.de>
2024-12-03 15:18:39 +0100

    - use ranges with regex iterator

--- AdventOfCode2024/AdventOfCode2024/day3/part1/day3_part1.cpp
+++ AdventOfCode2024/AdventOfCode2024/day3/part1/day3_part1.cpp
@@ -1,5 +1,6 @@
 #include "../day3_part1.h"
 
+#include <algorithm>
 #include <regex>
 
 int perform_valid_multiplications(const std::string& input)
@@ -7,19 +8,15 @@ int perform_valid_multiplications(const std::string& input)
     const auto find_valid_multiplication = std::regex(R"(mul\(\d+,\d+\))");
     const auto find_number = std::regex(R"(\d+)");
 
-    auto sum = 0;
-    for (auto mul_it = std::sregex_iterator(input.begin(), input.end(), find_valid_multiplication); mul_it != std::sregex_iterator(); ++mul_it)
-    {
-        const auto& match = mul_it->str();
-
-        auto prod = 1;
-        for (auto num_it = std::sregex_iterator(match.begin(), match.end(), find_number); num_it != std::sregex_iterator(); ++num_it)
+    const auto regex_range = std::ranges::subrange(std::sregex_iterator(input.begin(), input.end(), find_valid_multiplication), std::sregex_iterator());
+    return std::ranges::fold_left(regex_range, 0, [&](const int sum, const auto& match)
         {
-            prod *= std::stoi(num_it->str());
-        }
+            const auto& mul_str = match.str();
+            const auto match_range = std::ranges::subrange(std::sregex_iterator(mul_str.begin(), mul_str.end(), find_number), std::sregex_iterator());
 
-        sum += prod;
-    }
-
-    return sum;
+            return sum + std::ranges::fold_left(match_range, 1, [](const int prod, const auto& num)
+                {
+                    return prod * std::stoi(num.str());
+                });
+        });
 }
