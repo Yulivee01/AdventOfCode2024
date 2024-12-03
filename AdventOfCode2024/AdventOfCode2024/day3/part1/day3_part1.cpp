6d6931d7aa25086da6d05c6c6222b56000981acb
Tina Ulbrich <tina.ulbrich@web.de>
2024-12-03 17:52:17 +0100

    more ranges

--- AdventOfCode2024/AdventOfCode2024/day3/part1/day3_part1.cpp
+++ AdventOfCode2024/AdventOfCode2024/day3/part1/day3_part1.cpp
@@ -1,6 +1,8 @@
 #include "../day3_part1.h"
 
 #include <algorithm>
+#include <functional>
+#include <ranges>
 #include <regex>
 
 int perform_valid_multiplications(const std::string& input)
@@ -9,14 +11,10 @@ int perform_valid_multiplications(const std::string& input)
     const auto find_number = std::regex(R"(\d+)");
 
     const auto regex_range = std::ranges::subrange(std::sregex_iterator(input.begin(), input.end(), find_valid_multiplication), std::sregex_iterator());
-    return std::ranges::fold_left(regex_range, 0, [&](const int sum, const auto& match)
+    return std::ranges::fold_left(regex_range | std::views::transform([&](const auto& match)
         {
             const auto& mul_str = match.str();
             const auto match_range = std::ranges::subrange(std::sregex_iterator(mul_str.begin(), mul_str.end(), find_number), std::sregex_iterator());
-
-            return sum + std::ranges::fold_left(match_range, 1, [](const int prod, const auto& num)
-                {
-                    return prod * std::stoi(num.str());
-                });
-        });
+            return std::ranges::fold_left(match_range | std::views::transform([](const auto& num) {return std::stoi(num.str()); }), 1, std::multiplies{});
+        }), 0, std::plus{});
 }
