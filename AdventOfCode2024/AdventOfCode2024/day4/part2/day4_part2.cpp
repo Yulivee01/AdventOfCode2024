bae0a783e07008496d3e1884f18a2174dd027b9d
Tina Ulbrich <tina.ulbrich@web.de>
2024-12-09 22:18:31 +0100

    clean up part 2 a little

--- AdventOfCode2024/AdventOfCode2024/day4/part2/day4_part2.cpp
+++ AdventOfCode2024/AdventOfCode2024/day4/part2/day4_part2.cpp
@@ -2,17 +2,9 @@
 
 auto search_around_a(const ptrdiff_t i0, const ptrdiff_t j0, const std::vector<std::string>& input)
 {
-    const auto in_len = std::ssize(input);
-    const auto ip1 = i0 + 1;
-    const auto im1 = i0 - 1;
-
-    const auto str_len = std::ssize(input[i0]);
-    const auto jp1 = j0 + 1;
-    const auto jm1 = j0 - 1;
-
     int count = 0;
-    if ((std::string{ input[im1][jm1],input[i0][j0], input[ip1][jp1] }.contains("MAS") || std::string{ input[im1][jm1],input[i0][j0], input[ip1][jp1] }.contains("SAM"))
-        && (std::string{ input[im1][jp1], input[i0][j0], input[ip1][jm1] }.contains("MAS") || std::string{ input[im1][jp1], input[i0][j0], input[ip1][jm1] }.contains("SAM")))
+    if ((std::string{ input[i0 - 1][j0 - 1],input[i0][j0], input[i0 + 1][j0 + 1] }.contains("MAS") || std::string{ input[i0 - 1][j0 - 1],input[i0][j0], input[i0 + 1][j0 + 1] }.contains("SAM"))
+        && (std::string{ input[i0 - 1][j0 + 1], input[i0][j0], input[i0 + 1][j0 - 1] }.contains("MAS") || std::string{ input[i0 - 1][j0 + 1], input[i0][j0], input[i0 + 1][j0 - 1] }.contains("SAM")))
     {
         ++count;
     }
