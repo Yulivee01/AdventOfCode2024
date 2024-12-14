bb1b38880b4595648a575c59f70fb29a889fb55a
Tina Ulbrich <tina.ulbrich@web.de>
2024-12-14 18:20:05 +0100

    - more ranges

--- AdventOfCode2024/AdventOfCode2024/day4/part2/day4_part2.cpp
+++ AdventOfCode2024/AdventOfCode2024/day4/part2/day4_part2.cpp
@@ -1,36 +1,34 @@
 #include "day4_part2.h"
 
+#include <algorithm>
+#include <ranges>
+
 namespace
 {
     auto search_around_a(const ptrdiff_t i, const ptrdiff_t j, const std::vector<std::string>& input)
     {
-        int count = 0;
         const auto diagonal_left_right = std::string{ input[i - 1][j - 1], input[i][j], input[i + 1][j + 1] };
         const auto diagonal_right_left = std::string{ input[i - 1][j + 1], input[i][j], input[i + 1][j - 1] };
 
         if ((diagonal_left_right.contains("MAS") || diagonal_left_right.contains("SAM"))
             && (diagonal_right_left.contains("MAS") || diagonal_right_left.contains("SAM")))
         {
-            ++count;
+            return 1;
         }
 
-        return count;
+        return 0;
     }
 }
 
 int count_x_mas(const std::vector<std::string>& input)
 {
-    int count = 0;
-    for (ptrdiff_t i = 1; i < std::ssize(input) - 1; ++i)
-    {
-        for (ptrdiff_t j = 1; j < std::ssize(input[i]) - 1; ++j)
-        {
-            if (input[i][j] == 'A')
+    return std::ranges::fold_left(std::views::cartesian_product(
+        std::views::iota(ptrdiff_t{ 1 }, std::ssize(input) - 1),
+        std::views::iota(ptrdiff_t{ 1 }, std::ssize(input.front()) - 1))
+        | std::views::transform([&](const auto& coordinates)
             {
-                count += search_around_a(i, j, input);
-            }
-        }
-    }
-
-    return count;
+                const auto [i, j] = coordinates;
+                if (input[i][j] == 'A') return search_around_a(i, j, input);
+                return 0;
+            }), 0, std::plus{});
 }
