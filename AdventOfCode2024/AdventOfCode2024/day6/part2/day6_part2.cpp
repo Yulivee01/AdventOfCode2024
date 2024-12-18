4eb1226a73c24a79bf13eb436a9a2c179c395032
Tina Ulbrich <tina.ulbrich@web.de>
2024-12-18 21:12:25 +0100

    add comments and clean up

--- AdventOfCode2024/AdventOfCode2024/day6/part2/day6_part2.cpp
+++ AdventOfCode2024/AdventOfCode2024/day6/part2/day6_part2.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <iostream>
 #include <optional>
-#include <ranges>
 
 namespace
 {
@@ -31,40 +30,40 @@ int count_loops(std::vector<std::string>& map)
     const auto start_position = find_start(map);
 
     auto loops = 0;
+    // set an obstacle at every position of the map and check if we are in a loop
     for (ptrdiff_t i = 0; i < std::ssize(map); ++i)
     {
         for (ptrdiff_t j = 0; j < std::ssize(map.front()); ++j)
         {
+            // set start position and direction
             auto position = start_position;
             auto dir = direction::up;
 
+            // initialize state
             auto state = std::vector<std::vector<std::pair<std::optional<direction>, int>>>(map.size());
             for (auto& s : state) s.resize(map.front().size());
 
+            // skip if i and y are at start position -> no obstacle can be placed at start position
             if (start_position.x == i && start_position.y == j) break;
 
+            // save original value and set obstacle
             auto org_val = map[i][j];
             map[i][j] = '#';
 
-            //std::cout << " i " << i << " j " << j << std::endl;
+            // test for loops with obstacle at i/j
             while (position.x > -1 && position.x < std::ssize(map) && position.y > -1 && position.y < std::ssize(map.front()))
             {
-                //if (i == 4 && j == 8)
-                //{
-                //    std::cout
-                //        << "x " << position.x
-                //        << " y " << position.y
-                //        << " map[position.x][position.y] " << map[position.x][position.y]
-                //        << " state[position.x][position.y] " << (state[position.x][position.y].has_value() ? static_cast<int>(*state[position.x][position.y]) : -1)
-                //        << std::endl;
-                //}
-
+                // hit obstacle
                 if (map[position.x][position.y] == '#')
                 {
+                    // we are directly on the obstacle so we have to move back a step where we came from
                     correct_position(position, dir);
+
+                    // rotate 90 degrees to the right
                     rotate(dir);
                 }
 
+                // test if we are in a loop by checking if we have been here before, going in the same direction or if we are going back and forth
                 if (state[position.x][position.y].first.has_value()
                     && (state[position.x][position.y].first == dir
                         || (state[position.x][position.y].first == opposite(dir) && state[position.x][position.y].second > 2)))
@@ -73,13 +72,15 @@ int count_loops(std::vector<std::string>& map)
                     break;
                 }
 
+                // set state and update counter
                 state[position.x][position.y].first = dir;
                 state[position.x][position.y].second++;
-                move(position, dir);
 
-                //if (std::ranges::all_of(state | std::views::join, [](const auto opt) {return opt.has_value(); })) break;
+                // move forward
+                move(position, dir);
             }
 
+            // reset map
             map[i][j] = org_val;
         }
     }
