ae73f17ac4de7f93e6befb88e13d099f4fe5b8ac
Tina Ulbrich <tina.ulbrich@web.de>
2024-12-28 23:03:14 +0100

    adding tons of comments

--- AdventOfCode2024/AdventOfCode2024/day8/part1/day8_part1.cpp
+++ AdventOfCode2024/AdventOfCode2024/day8/part1/day8_part1.cpp
@@ -17,53 +17,61 @@ size_t count_antinodes(const std::string& input, const ptrdiff_t row_size)
     for (const auto symbol : unique_symbols)
     {
         // find all indices of one antenna
-        auto indices_symbol = std::vector<ptrdiff_t>();
-        indices_symbol.reserve(input.size());
-        for (const auto [index, value] : std::views::enumerate(input))
-        {
-            if (value == symbol)
-            {
-                indices_symbol.push_back(index);
-            }
-        }
+        const auto indices_symbol = std::views::enumerate(input)
+            | std::views::filter([&](const auto& pair) { return std::get<1>(pair) == symbol; })
+            | std::views::transform([](const auto& pair) { return std::get<0>(pair); })
+            | std::ranges::to<std::vector>();
 
-        for (size_t i = 2; i < indices_symbol.size() + 1; ++i)
+        for (const auto i : std::views::iota(size_t{ 2 }, indices_symbol.size() + 1))
         {
+            // iterate over all combinations of two antennas
             for (const auto subrange : indices_symbol | std::views::slide(i))
             {
-                const auto distance = subrange.back() - subrange.front();
+                const auto distance_between_antennas = subrange.back() - subrange.front();
 
-                const auto front_idx_in_row = subrange.front() % row_size;
-                const auto lower_index = subrange.front() - distance;
-                const auto lower_idx_in_row = lower_index % row_size;
+                const auto front_antenna_idx_in_row = subrange.front() % row_size;
+                const auto lower_antinode_index = subrange.front() - distance_between_antennas;
+                const auto lower_antinode_idx_in_row = lower_antinode_index % row_size;
 
-                const auto back_idx_in_row = subrange.back() % row_size;
-                const auto upper_index = subrange.back() + distance;
-                const auto upper_index_in_row = upper_index % row_size;
+                const auto back_antinode_idx_in_row = subrange.back() % row_size;
+                const auto upper_antinode_index = subrange.back() + distance_between_antennas;
+                const auto upper_antinode_idx_in_row = upper_antinode_index % row_size;
 
-                if (lower_index > 0)
+                // try to place an antinode in line of two antennas
+                // try to place the lower antinode
+                // overall index cannot be out of bounds
+                if (lower_antinode_index > 0)
                 {
-                    // direction is from upper to lower right
-                    if (front_idx_in_row < back_idx_in_row)
+                    // direction of the antennas is from upper left to lower right
+                    if (front_antenna_idx_in_row < back_antinode_idx_in_row)
                     {
-                        if (front_idx_in_row >= lower_idx_in_row) antinodes[lower_index] = '#';
+                        // checking if we are in bounds of the row, not going out of the left bound
+                        if (front_antenna_idx_in_row >= lower_antinode_idx_in_row) antinodes[lower_antinode_index] = '#';
                     }
                     else
                     {
-                        if (front_idx_in_row <= lower_idx_in_row) antinodes[lower_index] = '#';
+                        // checking if we are in bounds of the row
+                        // when we are going from upper right to lower left, the condition is switched
+                        // checking if we are not going out of the right bound
+                        if (front_antenna_idx_in_row <= lower_antinode_idx_in_row) antinodes[lower_antinode_index] = '#';
                     }
                 }
 
-                if (upper_index < std::ssize(input) - 1)
+                // try to place the upper antinode
+                if (upper_antinode_index < std::ssize(input) - 1)
                 {
-                    // direction is from upper to lower right
-                    if (front_idx_in_row < back_idx_in_row)
+                    /// direction of the antennas is from upper left to lower right
+                    if (front_antenna_idx_in_row < back_antinode_idx_in_row)
                     {
-                        if (back_idx_in_row <= upper_index_in_row) antinodes[upper_index] = '#';
+                        // checking if we are in bounds of the row, not going out of the right bound
+                        if (back_antinode_idx_in_row <= upper_antinode_idx_in_row) antinodes[upper_antinode_index] = '#';
                     }
                     else
                     {
-                        if (back_idx_in_row >= upper_index_in_row) antinodes[upper_index] = '#';
+                        // checking if we are in bounds of the row
+                        // when we are going from upper right to lowe left, the condition is switched
+                        // checking if we are not going out of the left bound
+                        if (back_antinode_idx_in_row >= upper_antinode_idx_in_row) antinodes[upper_antinode_index] = '#';
                     }
                 }
             }
